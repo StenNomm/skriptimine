e5f77217b882f5ae53aedd8aa4b2fb76cf6148a0
Sten Nomm <sten.nomm@khk.ee>
2019-05-06 05:56:39 -0400

    yl3.sh

---
+++ praks4a/yl3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+#backupi koostaja
+tar -czf /home/user/skriptimine/praks4a/backup/test.tar.gz /home/user/skriptimine/praks4a/test
+echo "kataloogi /home/user/skriptimine/praks4a/test/ backup-i nimi on test.tar.gz ja ta asub /home/user/skriptimine/praks4a/backup
+#
+#lõpp
